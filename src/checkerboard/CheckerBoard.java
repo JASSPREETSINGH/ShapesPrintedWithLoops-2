f7e4c078157d83248370768c520479e91eaaed4a
Jaspreet Singh <>
2018-01-01 16:53:24 -0500

    CheckerBoard

--- src/checkerboard/CheckerBoard.java
+++ src/checkerboard/CheckerBoard.java
@@ -15,7 +15,15 @@ public class CheckerBoard {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int size = 7;    // size of the board
+      for (int row = 1; row <= size; ++row) {  // Use row and col, NOT i and j, or x and y
+           if ((row % 2) == 0) {   // row 2, 4, 6, 8
+            System.out.print(" ");
+         }
+         for (int col = 1; col<=size; ++col) {
+           System.out.print("# ");   // print # and a space, without newline
+         }
+        System.out.println();     // print a newline
+      } // TODO code application logic here
     }
-    
 }
